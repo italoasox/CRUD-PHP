99fb7e79e99b55511af018bbec06c294fff1fcf8
italoasox <italo_intz@outlook.com>
2025-09-01 20:05:35 -0300

    CRUD-PHP

--- acoes.php
+++ acoes.php
@@ -10,15 +10,48 @@ if (isset($_POST['create_usuario'])) {
     $sql = "INSERT INTO usuarios (nome, email, data_nascimento, senha) VALUES ('$nome', '$email', '$data_nascimento', '$senha')";
 
     mysqli_query(mysql: $conexao, query: $sql);
-}
 
-if (mysqli_affected_rows(mysql: $conexao) > 0) {
+
+
+
+    if (mysqli_affected_rows(mysql: $conexao) > 0) {
     $_SESSION['mensagem'] = 'Usuário criado com sucesso';
     header(header: 'Location: index.php');
     exit;
-} else{
+}   else{
     $_SESSION['mensagem'] = 'Usuário não foi criado';
     header(header: 'Location: index.php');
     exit;
+}
+
+}
+
+if (isset($_POST['update_usuario'])) {
+    $usuario_id = mysqli_real_escape_string($conexao, $_POST['usuario_id']);
+
+    $nome = mysqli_real_escape_string($conexao, trim($_POST['nome']));
+    $email = mysqli_real_escape_string($conexao,trim($_POST['email']));
+    $data_nascimento = mysqli_real_escape_string($conexao,trim($_POST['data_nascimento']));
+    $senha = mysqli_real_escape_string($conexao, trim($_POST['senha']));
+
+    $sql = "UPDATE usuarios SET nome = '$nome', email = '$email', data_nascimento = '$data_nascimento'";
+
+    if (!empty($senha)) {
+        $sql .= ", senha='" . password_hash($senha, PASSWORD_DEFAULT) . "'";
+    }
+
+    $sql .= " WHERE  id = '$usuario_id'";
+
+    mysqli_query($conexao, $sql);
+
+if (mysqli_affected_rows($conexao) > 0) {
+    $_SESSION['mensagem'] = 'Usuário atualizado com sucesso';
+    header(header: 'Location: index.php');
+    exit;
+} else{
+    $_SESSION['mensagem'] = 'Usuário não foi atualizado';
+    header(header: 'Location: index.php');
+    exit;
+    }
 }
 ?>
--- mensagem.php
+++ mensagem.php
@@ -1,8 +1,8 @@
 <?php
-if (isset($_SESSION['mensagem'])):
+    if (isset($_SESSION['mensagem'])):
 ?>
 
-<div class="alert alert-warning alert-dismissible fade show" role="alert">
+<div class="alert alert-warning alert-dismissble fade show" role="alert">
     <?= $_SESSION['mensagem']; ?>
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
